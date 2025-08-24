c509beba9fd4fb9106523455dca2ceaf9736bf18
Isaac <sebasproano917@gmail.com>
2025-08-23 21:34:15 -0500

    first commit

---
+++ utilitarios/saludo.js
@@ -0,0 +1,36 @@
+saludar = function () {
+  //recueperar el valor de la caja de texto
+  let nombre;
+  nombre = recuperarTexto("txtNombre");
+  let apellido;
+  apellido = recuperarTexto("txtApellido");
+  let edad;
+  edad = recuperarInt("txtEdad");
+  let estatura;
+  estatura=recuperarFloat("txtEstatura")
+};
+
+recuperarTexto = function (idComponente) {
+  let componente;
+  let valorIngresado;
+  componente = document.getElementById(idComponente);
+  valorIngresado = componente.value;
+
+  return valorIngresado;
+};
+
+recuperarInt = function (idComponente) {
+  let valorCaja;
+  let valorEntero;
+  valorCaja = recuperarTexto(idComponente);
+  valorEntero = parseInt(valorCaja);
+  return valorEntero;
+};
+
+recuperarFloat = function (idComponente) {
+  let valorCaja;
+  let valorFloat;
+  valorCaja = recuperarTexto(idComponente);
+  valorFloat = parseFloat(valorCaja);
+  return valorFloat;
+};
